5e22fb43005c0727a9d85eb8704d18f1c3a802c3
Ernesto <ernesto.cuapantecatl@bunsan.io>
2026-01-21 19:47:13 -0600

    Add more tests for LOG processing results

--- test/log_processor_test.exs
+++ test/log_processor_test.exs
@@ -8,6 +8,40 @@ defmodule FileProcessor.LogProcessorTest do
     }
   end
 
+  describe "process/1" do
+    test "returns a two-element tuple with :ok and a map", %{valid_log: valid_log} do
+      assert {:ok, metrics} = FileProcessor.LogProcessor.process(valid_log)
+      assert is_map(metrics)
+    end
+
+    test "returns a map with expected keys", %{valid_log: valid_log} do
+      {:ok, metrics} = FileProcessor.LogProcessor.process(valid_log)
+      expected_keys = [
+        :total_entries,
+        :level_distribution,
+        :most_problematic_component,
+        :frequent_error_pattern,
+        :peak_log_hour,
+        :errors_found,
+        :error_details
+      ]
+
+      assert Enum.sort(Map.keys(metrics)) == Enum.sort(expected_keys)
+    end
+
+    test "returns metrics with the correct data type", %{valid_log: valid_log} do
+      {:ok, metrics} = FileProcessor.LogProcessor.process(valid_log)
+
+      assert is_integer(metrics.total_entries)
+      assert is_list(metrics.level_distribution)
+      assert is_binary(metrics.most_problematic_component)
+      assert is_binary(metrics.frequent_error_pattern)
+      assert is_binary(metrics.peak_log_hour)
+      assert is_integer(metrics.errors_found)
+      assert is_list(metrics.error_details)
+    end
+  end
+
   describe "process/1 with valid files" do
     test "returns metrics with the exact expected values", %{valid_log: valid_log} do
       {:ok, metrics} = FileProcessor.LogProcessor.process(valid_log)

711f6758d443e05ce7ca6e9eade3a5bbcbd42813
Ernesto <ernesto.cuapantecatl@bunsan.io>
2026-01-21 19:46:39 -0600

    Add more tests for JSON processing results

--- test/json_processor_test.exs
+++ test/json_processor_test.exs
@@ -4,8 +4,49 @@ defmodule FileProcessor.JsonProcessorTest do
   setup do
     %{
       valid_json: "data/valid/usuarios.json",
-      error_json: "data/error/usuarios_sucio.json"
+      error_json: "data/error/usuarios_sucio.json",
+      malformed_json: "data/error/usuarios_malformado.json"
     }
+  end
+
+  describe "process/1" do
+    test "returns a two-element tuple with :ok and a map", %{valid_json: valid_json} do
+      assert {:ok, metrics} = FileProcessor.JsonProcessor.process(valid_json)
+      assert is_map(metrics)
+    end
+
+    test "returns a map with expected keys", %{valid_json: valid_json} do
+      {:ok, metrics} = FileProcessor.JsonProcessor.process(valid_json)
+      expected_keys = [
+        :total_users,
+        :active_users,
+        :active_percent,
+        :avg_session_duration,
+        :total_pages_visited,
+        :top_5_actions,
+        :peak_hour,
+        :total_sessions,
+        :errors_found,
+        :error_details
+      ]
+
+      assert Enum.sort(Map.keys(metrics)) == Enum.sort(expected_keys)
+    end
+
+    test "returns metrics with the correct data type", %{valid_json: valid_json} do
+      {:ok, metrics} = FileProcessor.JsonProcessor.process(valid_json)
+
+      assert is_integer(metrics.total_users)
+      assert is_integer(metrics.active_users)
+      assert is_float(metrics.active_percent)
+      assert is_float(metrics.avg_session_duration)
+      assert is_integer(metrics.total_pages_visited)
+      assert is_list(metrics.top_5_actions)
+      assert is_binary(metrics.peak_hour)
+      assert is_integer(metrics.total_sessions)
+      assert is_integer(metrics.errors_found)
+      assert is_list(metrics.error_details)
+    end
   end
 
   describe "process/1 with valid files" do
@@ -50,6 +91,11 @@ defmodule FileProcessor.JsonProcessorTest do
         "Session duration negative or invalid for User: 1",
         "Session duration negative or invalid for User: 2"
       ]
+    end
+
+    test "Handles a malformed JSON file and returns an error tuple", %{malformed_json: malformed_json} do
+      assert {:error, reason} = FileProcessor.JsonProcessor.process(malformed_json)
+      assert reason == "Malformed JSON file"
     end
   end
 end

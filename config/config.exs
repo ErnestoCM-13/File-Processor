9d1b5ce7127236b4093bf4f9a4af250e306c69da
YaanitGran <yaanit.granados@bunsan.io>
2026-02-11 11:42:50 -0600

    Adding upload system for csv, log and json files

--- config/config.exs
+++ config/config.exs
@@ -60,6 +60,9 @@ config :logger, :default_formatter,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+config :mime, :types, %{
+  "text/plain" => ["log"]
+}
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{config_env()}.exs"
--- lib/file_processor_web/live/processor_live.ex
+++ lib/file_processor_web/live/processor_live.ex
@@ -1,24 +1,41 @@
 defmodule FileProcessorWeb.ProcessorLive do
   use FileProcessorWeb, :live_view
 
-  # Al cargar la página, definimos el estado inicial
+
   def mount(_params, _session, socket) do
-    {:ok, assign(socket,
-      page_title: "File Processor",
-      mode: "sequential", # Modo seleccionado por defecto
-      workers: 4,
-      timeout: 4
-    )}
+    {:ok,
+     socket
+     |> assign(
+       page_title: "File Processor",
+       mode: "sequential",
+       workers: 4,
+       timeout: 4
+     )
+     # Upload files
+     |> allow_upload(:files,
+       accept: ~w(.csv .json .log),
+       max_entries: 10,
+       auto_upload: true
+     )}
   end
 
-  # Esta función captura el clic en los modos de procesamiento
+  # captures click
   def handle_event("select-mode", %{"mode" => selected_mode}, socket) do
     {:noreply, assign(socket, mode: selected_mode)}
   end
 
-  # Aquí manejaremos el inicio del procesamiento después
+  # handle the uploading process
+  def handle_event("validate", _params, socket) do
+    {:noreply, socket}
+  end
+
+  def handle_event("cancel-upload", %{"ref" => ref}, socket) do
+    {:noreply, cancel_upload(socket, :files, ref)}
+  end
+
+  # Process handle
   def handle_event("start-processing", _params, socket) do
-    # Por ahora solo mandamos un log
+
     IO.puts("Iniciando procesamiento en modo: #{socket.assigns.mode}")
     {:noreply, socket}
   end

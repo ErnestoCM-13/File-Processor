5dc4919a0f1fef077e767848cefdf50bfc547593
Ernesto <ernesto.cuapantecatl@bunsan.io>
2026-02-20 19:14:16 -0600

    Update benchmark mode measurement for new 'max workers' functionality

--- lib/benchmark.ex
+++ lib/benchmark.ex
@@ -21,14 +21,15 @@ defmodule Benchmark do
   ## Benchmark metrics map
   - `:time` - total execution time in seconds
   - `:memory_mb` - approximate memory usage in MB
-  - `:processes` - number of processes used (extracted from `:processes_used` if present)
+  - `:processes` - number of processes used
+  - `:max_processes_used` - maximum number of processes used at the same time
   """
   def measure(function) when is_function(function) do
     start_time = System.monotonic_time(:microsecond)
 
     start_memory = total_process_memory()
 
-    funtion_results = function.()
+    function_results = function.()
 
     end_time = System.monotonic_time(:microsecond)
 
@@ -38,15 +39,25 @@ defmodule Benchmark do
 
     total_time_seconds = (end_time - start_time) / 1_000_000
 
-    processes_count = if is_map(funtion_results), do: Map.get(funtion_results, :processes_used, 1), else: 1
+    {processes_total, max_processes_used} =
+      if is_map(function_results) do
+        processes_used = Map.get(function_results, :processes_used, 1)
+        max_workers = Map.get(function_results, :max_workers, 1)
+
+        final_max_workers = if max_workers <= processes_used, do: max_workers, else: processes_used
+        {processes_used, final_max_workers}
+      else
+        {1, 1}
+      end
 
     benchmark_metrics = %{
       time: total_time_seconds,
       memory_mb: memory_used_mb,
-      processes: processes_count
+      processes: processes_total,
+      max_processes_used: max_processes_used
     }
 
-    {benchmark_metrics, funtion_results}
+    {benchmark_metrics, function_results}
   end
 
   @doc """
@@ -64,6 +75,7 @@ defmodule Benchmark do
       sequential_time: sequential_metrics.time,
       parallel_time: parallel_metrics.time,
       improvement: Float.round(sequential_metrics.time / parallel_metrics.time, 2),
+      max_processes_used: parallel_metrics.max_processes_used,
       processes: parallel_metrics.processes,
       memory_max: parallel_metrics.memory_mb
     }
